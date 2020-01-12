4717044aeb146559b3d17ae207cb67b18d7c3af5
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2020-01-11 23:52:24 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -14,18 +14,21 @@ function frames(variable){
         document.getElementById("enframe").src="pages/submitted.html";
     }
     else if (variable==5){
-        document.getElementById("enframe").src="pages/talks.html";
+        document.getElementById("enframe").src="pages/projects.html";
     }
     else if (variable==6){
-        document.getElementById("enframe").src="pages/students.html";  
+        document.getElementById("enframe").src="pages/talks.html";
     }
     else if (variable==7){
+        document.getElementById("enframe").src="pages/students.html";  
+    }
+    else if (variable==8){
         document.getElementById("enframe").src="pages/teaching.html";
     }  
-    else if (variable==8){
+    else if (variable==9){
         document.getElementById("enframe").src="pages/gnu-linux-software.html";
     }  
-    else if (variable==9){
+    else if (variable==10){
         document.getElementById("enframe").src="pages/links.html";
     }  
 } 

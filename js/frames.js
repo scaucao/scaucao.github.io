fa9c8f3ec4fe20ea9e6cc16d670d9e5048f62c32
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2021-04-29 16:41:19 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,31 +1,41 @@
    
 function frames(variable){
-    document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="650";
     if (variable==1){
         document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="650";
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/about_me.html";
+        document.getElementById("enframe").height="680";
     }
     else if (variable==3){
         document.getElementById("enframe").src="pages/publications.html";
+        document.getElementById("enframe").height="1310";
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/projects.html";
+        document.getElementById("enframe").height="680";
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/talks.html";
+        document.getElementById("enframe").height="1000";
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/students.html";  
+        document.getElementById("enframe").height="680";
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/teaching.html";
+        document.getElementById("enframe").height="680";
     }  
     else if (variable==8){
         document.getElementById("enframe").src="pages/gnu-linux-software.html";
+        document.getElementById("enframe").height="680";
     }  
     else if (variable==9){
         document.getElementById("enframe").src="pages/links.html";
+        document.getElementById("enframe").height="680";
     }  
 } 

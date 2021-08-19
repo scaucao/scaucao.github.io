09fd3f245d6232a16ce0c2bdc350a5ca531e4083
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2021-08-19 15:18:14 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,14 +1,14 @@
    
 function frames(variable){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     if (variable==1){
         document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/about_me.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }
     else if (variable==3){
         document.getElementById("enframe").src="pages/publications.html";
@@ -16,7 +16,7 @@ function frames(variable){
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/projects.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/talks.html";
@@ -24,18 +24,18 @@ function frames(variable){
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/students.html";  
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/teaching.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }  
     else if (variable==8){
         document.getElementById("enframe").src="pages/gnu-linux-software.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }  
     else if (variable==9){
         document.getElementById("enframe").src="pages/links.html";
-        document.getElementById("enframe").height="620";
+        document.getElementById("enframe").height="630";
     }  
 } 

98c83ac99a0e04b09b49d57abf4d7f9166c12c13
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2023-03-21 20:44:56 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -16,7 +16,7 @@ function frames(variable){
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/publications.html";
-        document.getElementById("enframe").height="1600";
+        document.getElementById("enframe").height="1700";
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/talks.html";

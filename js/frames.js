9c1a5b3812ef4f83d3e3d56e6559ff56073fa7ee
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2024-09-26 19:20:10 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -28,7 +28,7 @@ function frames(variable){
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/worth-noting.html";
-        document.getElementById("enframe").height="630";
+        document.getElementById("enframe").height="680";
     }  
     else if (variable==8){
         document.getElementById("enframe").src="pages/teaching.html";

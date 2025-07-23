e6f4675f401b392b3b56aa9bd10c280a9a1933fe
Sergio Caucao <49847005+scaucao@users.noreply.github.com>
2025-07-22 22:27:25 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -20,7 +20,7 @@ function frames(variable){
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/students.html";  
-        document.getElementById("enframe").height="910";
+        document.getElementById("enframe").height="1050";
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/postdocs-RAs.html";  
